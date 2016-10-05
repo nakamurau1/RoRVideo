04bb8d791d5ee2f37663789645fbd8e01490e345
中村悠一 <nakamurau1@nakamurayuuichi-no-MacBook-Pro.local>
2016-10-05 21:19:24 +0900

    title update

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,12 +2,12 @@ module ApplicationHelper
 
   # ページごとの完全なタイトルを返します。
   def full_title(page_title)
-    base_title = "rovideo エロ動画まとめ"
+    base_title = "RoRVideo -Ruby on Railsの動画検索サービス-"
     if page_title.empty?
       base_title
     else
       "#{page_title} | #{base_title}"
     end
   end
-  
+
 end
